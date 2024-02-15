f0bada47859b6cdb513a20f361194635b0d494f9
yugure <109891005+yugure-orca@users.noreply.github.com>
2024-02-15 12:29:03 +0900

    add order by option on whirlpool list page

--- src/pages/whirlpool/List.svelte
+++ src/pages/whirlpool/List.svelte
@@ -1,32 +1,75 @@
 <script lang="ts">
+  import InputRadioGroup from "../../components/InputRadioGroup.svelte";
+  import { getWhirlpoolList, WhirlpoolListEntry, whirlpoolListEntryCmp } from "../../libs/orcaapi";
+
+  enum OrderBy {
+    volumeDesc = "day $volume (desc)",
+    volumeAsc = "day $volume (asc)",
+    tvlDesc = "$TVL (desc)",
+    tvlAsc = "$TVL (asc)",
+    nameAsc = "name (asc)",
+  }
+
+  const orderByOptions = Object.values(OrderBy);
+  let orderBy = orderByOptions.indexOf(OrderBy.volumeDesc);
+
+  function getComparator(orderBy: number): (a: WhirlpoolListEntry, b: WhirlpoolListEntry) => number {
+    switch (orderByOptions[orderBy]) {
+      case OrderBy.volumeDesc:
+        return (a, b) => b.usdVolumeDay.cmp(a.usdVolumeDay);
+      case OrderBy.volumeAsc:
+        return (a, b) => a.usdVolumeDay.cmp(b.usdVolumeDay);
+      case OrderBy.tvlDesc:
+        return (a, b) => b.usdTVL.cmp(a.usdTVL);
+      case OrderBy.tvlAsc:
+        return (a, b) => a.usdTVL.cmp(b.usdTVL);
+      case OrderBy.nameAsc:
+        return whirlpoolListEntryCmp;
+      default:
+        return (a, b) => 0;
+    }
+  }
+
   let filter;
   $: effectiveFilter = "";
-
-  import { getWhirlpoolList, WhirlpoolListEntry } from "../../libs/orcaapi";
-
-  $: whirlpoolListPromise = getFilteredWhirlpoolList(effectiveFilter);
+  $: whirlpoolListPromise = getFilteredWhirlpoolList(effectiveFilter, getComparator(orderBy));
 
   function onSubmit() {
     effectiveFilter = filter;
   }
 
-  async function getFilteredWhirlpoolList(filter: string): Promise<WhirlpoolListEntry[]> {
+  async function getFilteredWhirlpoolList(
+    filter: string,
+    orderByComparator: (a: WhirlpoolListEntry, b: WhirlpoolListEntry) => number,
+  ): Promise<WhirlpoolListEntry[]> {
     const list = await getWhirlpoolList();
-    return list.filter(
-      (p) => filter.length == 0 ||
-      p.name.toUpperCase().indexOf(filter.toUpperCase()) >= 0 ||
-      p.invertedName.toUpperCase().indexOf(filter.toUpperCase()) >= 0 ||
-      p.mintA.toBase58().indexOf(filter) >= 0 ||
-      p.mintB.toBase58().indexOf(filter) >= 0
-    );
+    return list
+      .filter(
+        (p) => filter.length == 0 ||
+        p.name.toUpperCase().indexOf(filter.toUpperCase()) >= 0 ||
+        p.invertedName.toUpperCase().indexOf(filter.toUpperCase()) >= 0 ||
+        p.mintA.toBase58().indexOf(filter) >= 0 ||
+        p.mintB.toBase58().indexOf(filter) >= 0
+      )
+      .sort(orderByComparator);
   }
 </script>
 
 <h2>🌀Whirlpool::list</h2>
-<form on:submit|preventDefault={onSubmit} style="margin-bottom: 1em;">
+<form on:submit|preventDefault={onSubmit} style="margin-bottom: 0em;">
   <input style="margin: 0.5em 0em;" bind:value={filter} type="text" size="64" placeholder="SOL/USDC or orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE" />
   <input type="submit" value="Set Filter!" />
 </form>
+<div style="margin-top: 0.5em; margin-left: 0em; margin-bottom: 1.0em; font-size: smaller; display: flex; flex-direction: row;">
+  <div>order by</div>
+  <div>
+  <InputRadioGroup
+    group="orderBy"
+    bind:selected={orderBy}
+    values={orderByOptions}
+  />
+  </div>
+</div>
 
 {#await whirlpoolListPromise}
   loading...
