f02ec9d775de01acf5f5c70fb13d1a552d9b4f86
yugure <109891005+yugure-orca@users.noreply.github.com>
2025-07-16 15:58:06 +0900

    add AdaptiveFeeTier 2048+ series

--- src/libs/whirlpool.ts
+++ src/libs/whirlpool.ts
@@ -15,13 +15,15 @@ import { TOKEN_2022_PROGRAM_ID } from "@solana/spl-token-2022";
 
 const NEIGHBORING_TICK_ARRAY_NUM = 9
 const ISOTOPE_TICK_SPACINGS = [1, 2, 4, 8, 16, 32, 64, 96, 128, 256, 512, 32896];
-const ADAPTIVE_FEE_TIER_INDEXES = [
+const PERMISSIONLESS_ADAPTIVE_FEE_TIER_INDEXES = [
   1025, 1026, 1027, 1028, 1029, 1030, 1031, 1032, 1033, 1034,
   1035, 1036, 1037, 1038, 1039, 1040, 1041, 1042, 1043, 1044,
   1045, 1046, 1047, 1048, 1049, 1050, 1051, 1052, 1053, 1054,
-  1055, 1056, 1057, 1058, 1059, 1060, 1061, 1062, 1063, 1064,
-  1065, 1066, 1067, 1068, 1069, 1070, 1071, 1072, 1073, 1074,
-  1075, 1076, 1077, 1078, 1079, 1080, 1081, 1082, 1083, 1084,
+];
+const PERMISSIONED_ADAPTIVE_FEE_TIER_INDEXES = [
+  2049, 2050, 2051, 2052, 2053, 2054, 2055, 2056, 2057, 2058,
+  2059, 2060, 2061, 2062, 2063, 2064, 2065, 2066, 2067, 2068,
+  2069, 2070, 2071, 2072, 2073, 2074, 2075, 2076, 2077, 2078,
 ];
 
 const DYNAMIC_TICK_ARRAY_DISCRIMINATOR = [17, 216, 246, 142, 225, 199, 218, 56];
@@ -572,7 +574,8 @@ type WhirlpoolsConfigDerivedInfo = {
   defaultProtocolFeeRate: Decimal,
   configExtension: PublicKey,
   feeTiers: InitializedFeeTier[],
-  adaptiveFeeTiers: InitializedAdaptiveFeeTier[],
+  adaptiveFeeTiersPermissionless: InitializedAdaptiveFeeTier[],
+  adaptiveFeeTiersPermissioned: InitializedAdaptiveFeeTier[],
 }
 
 type WhirlpoolsConfigInfo = {
@@ -610,21 +613,42 @@ export async function getWhirlpoolsConfigInfo(addr: Address): Promise<Whirlpools
     });
   });
 
-  const adaptiveFeeTierPubkeys: PublicKey[] = ADAPTIVE_FEE_TIER_INDEXES.map((feeTierIndex) => {
+  const adaptiveFeeTierPubkeys: PublicKey[] = [
+    ...PERMISSIONLESS_ADAPTIVE_FEE_TIER_INDEXES,
+    ...PERMISSIONED_ADAPTIVE_FEE_TIER_INDEXES,
+  ].map((feeTierIndex) => {
     return PDAUtil.getFeeTier(accountInfo.owner, pubkey, feeTierIndex).publicKey;
   });
 
   const adaptiveFeeTierAccountInfos = await connection.getMultipleAccountsInfo(adaptiveFeeTierPubkeys);
-  const adaptiveFeeTiers: InitializedAdaptiveFeeTier[] = [];
+  const adaptiveFeeTiersPermissionless: InitializedAdaptiveFeeTier[] = [];
+  const adaptiveFeeTiersPermissioned: InitializedAdaptiveFeeTier[] = [];
   adaptiveFeeTierAccountInfos.forEach((a, i) => {
     const adaptiveFeeTier = ParsableAdaptiveFeeTier.parse(adaptiveFeeTierPubkeys[i], a);
-    adaptiveFeeTiers.push({
-      feeTierIndex: ADAPTIVE_FEE_TIER_INDEXES[i],
-      pubkey: adaptiveFeeTierPubkeys[i],
-      isInitialized: adaptiveFeeTier !== null,
-      tickSpacing: adaptiveFeeTier === null ? undefined : adaptiveFeeTier.tickSpacing,
-      defaultBaseFeeRate: adaptiveFeeTier === null ? undefined : PoolUtil.getFeeRate(adaptiveFeeTier.defaultBaseFeeRate).toDecimal().mul(100),
-    });
+
+    const pubkey = adaptiveFeeTierPubkeys[i];
+    const isInitialized = adaptiveFeeTier !== null;
+    const tickSpacing = adaptiveFeeTier === null ? undefined : adaptiveFeeTier.tickSpacing;
+    const defaultBaseFeeRate = adaptiveFeeTier === null ? undefined : PoolUtil.getFeeRate(adaptiveFeeTier.defaultBaseFeeRate).toDecimal().mul(100);
+
+    if (i < PERMISSIONLESS_ADAPTIVE_FEE_TIER_INDEXES.length) {
+      adaptiveFeeTiersPermissionless.push({
+        feeTierIndex: PERMISSIONLESS_ADAPTIVE_FEE_TIER_INDEXES[i],
+        pubkey,
+        isInitialized,
+        tickSpacing,
+        defaultBaseFeeRate,
+      });
+    }
+    else {
+      adaptiveFeeTiersPermissioned.push({
+        feeTierIndex: PERMISSIONED_ADAPTIVE_FEE_TIER_INDEXES[i - PERMISSIONLESS_ADAPTIVE_FEE_TIER_INDEXES.length],
+        pubkey,
+        isInitialized,
+        tickSpacing,
+        defaultBaseFeeRate,
+      });
+    }
   });
   const configExtension = PDAUtil.getConfigExtension(accountInfo.owner, pubkey).publicKey;
 
@@ -635,7 +659,8 @@ export async function getWhirlpoolsConfigInfo(addr: Address): Promise<Whirlpools
       defaultProtocolFeeRate: PoolUtil.getProtocolFeeRate(whirlpoolsConfigData.defaultProtocolFeeRate).toDecimal().mul(100),
       configExtension,
       feeTiers,
-      adaptiveFeeTiers,
+      adaptiveFeeTiersPermissionless,
+      adaptiveFeeTiersPermissioned,
     }
   };
 }
